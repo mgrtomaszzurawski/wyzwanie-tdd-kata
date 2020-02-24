785310aa14d634dcbe03485ae9c73cb37279b765
Tomasz Zurawski <mgrtomaszzurawski@gmail.com>
2020-02-24 21:02:27 +0100

    Test for one value

--- src/test/java/wyzwanie/tddkata/CalculatorTest.java
+++ src/test/java/wyzwanie/tddkata/CalculatorTest.java
@@ -20,4 +20,18 @@ public class CalculatorTest {
         assertThat(result, equalTo(0));
     }
 
+    @Test
+    public void should_return_value_for_string_with_one_value() {
+        //given
+        Calculator calculatorUnderTest = new Calculator();
+        String stringWithValue= "4";
+
+
+        //when
+        Integer result = calculatorUnderTest.add(stringWithValue);
+
+        //then
+        assertThat(result, equalTo(4));
+    }
+
 }
